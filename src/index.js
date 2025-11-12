f65874e38426213b6d7553921d9c86e1cce75aa0
Nongten1010 <suvannadech.t@gmail.com>
2025-11-12 21:34:42 +0700

    update index.js

--- src/index.js
+++ src/index.js
@@ -6,5 +6,7 @@ app.get('/api/hello', (req, res) => {
   res.json({ msg: 'hello from backend' });
 });
 
+
+
 const PORT = process.env.PORT || 5000; // container ใช้ 5000
 app.listen(PORT, () => console.log('Backend listening on', PORT));
